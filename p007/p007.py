f14a1283e5177d6d40a33956c8fd7982ad42af55
Iñaki Silanes <isilanes@gmail.com>
2012-07-09 12:23:55 +0200

    p007: added a (quite naïve) solution

---
+++ p007/p007.py
@@ -0,0 +1,24 @@
+#------------------------------------------------------------------------------#
+
+def f1(nth):
+    primes = [2,3]
+    i = primes[-1] + 2
+    while len(primes) < nth:
+        is_prime = True
+        for prime in primes:
+            if not i % prime:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(i)
+
+        i += 2
+
+    return primes
+
+#------------------------------------------------------------------------------#
+
+for i in range(1):
+    res = f1(10001)
+
+print(res[-1])

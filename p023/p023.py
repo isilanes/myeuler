a0c8c9910e0e391aa2909dae5e998b558f46170a
isilanes <isilanes@gmail.com>
2012-07-19 20:58:44 +0200

    p023: add solution. Start putting enunciate in separate README

---
+++ p023/p023.py
@@ -0,0 +1,51 @@
+#-------------------------------------------------------------------------#
+
+def f1(max=28123):
+    
+    abundants = {}
+
+    # Sum of numbers that CAN'T be expressed as the sum of two 
+    # abundant numbers:
+    sum_cantbe = 1 # we will omit 1 below, which belongs here
+
+    # Modified from p021:
+    import math
+    for i in range(2,max+1):
+        divisors = {1:True}
+        sum_divisors = 1
+        for j in range(2,int(math.sqrt(i))+1):
+            if not i % j:
+                if not j in divisors:
+                    sum_divisors += j
+                    divisors[j] = True
+                ij = int(i/j)
+                if not ij in divisors:
+                    sum_divisors += ij
+                    divisors[ij] = True
+
+        if sum_divisors > i:
+            abundants[i] = True
+
+        # Regardless of i being abundant, take advantage of the
+        # loop and check if i is the sum of two abundant numbers.
+        # Those two hypothetical abundant numbers will be smaller
+        # than i itself, and thus already logged as such in 
+        # previous cycles of the loop.
+        canbe = False # can be expressed as the sum of two abundant numbers
+        for j in range(12,i-11): # 12 is smallest abundant number
+            if j in abundants:
+                ij = i - j
+                if ij in abundants:
+                    canbe = True
+                    break
+
+        if not canbe:
+            sum_cantbe += i
+
+    return sum_cantbe
+
+#-------------------------------------------------------------------------#
+
+res = f1(28123)
+
+print(res)

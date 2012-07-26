a880859474dadb30621b5f7e5724a446d22d4fb5
Iñaki Silanes <isilanes@gmail.com>
2012-07-26 14:51:01 +0200

    p032: added result

---
+++ p032/p032.py
@@ -0,0 +1,35 @@
+def f1():
+    import itertools
+
+    # M x N = O, M,N,O being 1-9 pandigital
+    pandi = '123456789'
+    lp = len(pandi)
+
+    pandis = {}
+    for m in range(1,lp/2+1):
+        for e in itertools.permutations(pandi, m):
+            M = ''.join(e)
+            rem = [ x for x in pandi if not x in M ]
+            for n in range(1,lp-2*m+1):
+                for el in itertools.permutations(rem, n):
+                    N = ''.join(el)
+                    REM = [ x for x in rem if not x in N ]
+                    MN = int(M) * int(N)
+                    MN = [ x for x in str(MN) ]
+                    MN.sort()
+                    if MN == REM:
+                        A = int(M)
+                        B = int(N)
+                        C = A*B
+                        pandis[C] = [A, B] 
+
+    sum = 0
+    for pan in pandis:
+        sum += pan
+
+    return sum
+
+#-------------------------------------------------------------------------#
+
+res = f1()
+print(res)

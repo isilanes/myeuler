cf4d5679f0419a5064fc22766ed05888718ec947
Iñaki Silanes <isilanes@gmail.com>
2012-07-30 15:04:35 +0200

    p035: started a second (sieve-based) method

--- p035/p035.py
+++ p035/p035.py
@@ -35,5 +35,34 @@ def isprime(m):
 
 #-------------------------------------------------------------------------#
 
-res = f1(1000000)
+def f2(max):
+    # First use sieve to get all composites below "max":
+    composites = {}
+    primes = [2]
+    for mult in range(3,max,2):
+        if not mult in composites:
+            primes.append(mult)
+            for i in range(mult*mult, max, 2*mult):
+                composites[i] = True
+
+    ncirculars = 0
+    # Check all primes to see if they are circular:
+    for prime in primes:
+        si = str(i)
+        if not '2' in si and not '4' in si and not '5' in si and not '6' in si and not '8' in 'si' and not '0' in si:
+            iscircular = True
+            for j in range(len(si)-1):
+                si = si[1:] + si[0]
+                ii = int(si)
+                if not isprime(ii):
+                    iscircular = False
+                    break
+            if iscircular:
+                ncirculars += 1
+
+    return ncirculars
+
+#-------------------------------------------------------------------------#
+
+res = f2(1000000)
 print(res)

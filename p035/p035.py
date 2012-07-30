86d285785b18bbd60e74e52d6bdd768125a368c4
Iñaki Silanes <isilanes@gmail.com>
2012-07-30 14:52:43 +0200

    p035: optimize a bit by rejecting any number with a least a digit in the (2,4,5,6,8) list (at least a rotation will not be prime)

--- p035/p035.py
+++ p035/p035.py
@@ -1,17 +1,18 @@
 def f1(max):
-    ncirculars = 1 # 2 is a circular prime
-    for i in range(3,max,2):
+    ncirculars = 4 # 2, 3, 5, 7 are circular primes
+    for i in range(11,max,2):
         if isprime(i):
             si = str(i)
-            iscircular = True
-            for j in range(len(si)-1):
-                si = si[1:] + si[0]
-                ii = int(si)
-                if not isprime(ii):
-                    iscircular = False
-                    break
-            if iscircular:
-                ncirculars += 1
+            if not '2' in si and not '4' in si and not '5' in si and not '6' in si and not '8' in 'si' and not '0' in si:
+                iscircular = True
+                for j in range(len(si)-1):
+                    si = si[1:] + si[0]
+                    ii = int(si)
+                    if not isprime(ii):
+                        iscircular = False
+                        break
+                if iscircular:
+                    ncirculars += 1
 
     return ncirculars
 

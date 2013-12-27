8717408d2346ce4ad8ec8b80933acb201b9ad25a
isilanes <isilanes@gmail.com>
2013-12-27 22:32:07 +0100

    Added solution to p078

--- p078/p078.py
+++ p078/p078.py
@@ -3,6 +3,42 @@
 def f0():
     print("--- f0 ---")
 
+    # From: http://en.wikipedia.org/wiki/Partition_(number_theory)
+
+    class MemoP(object):
+        
+        def __init__(self):
+            self.cache = {}
+
+        def call(self,N):
+            if not N in self.cache:
+                if N < 0:
+                    return 0
+
+                if N < 2:
+                    return 1
+
+                res = 0
+                for k in range(1,N):
+                    iplus = N - k*(3*k-1)/2
+                    iminus = N - k*(3*k+1)/2
+                    if iplus < 0:
+                        break
+                    res += (-1)**(k-1) * ( self.call(iplus) + self.call(iminus) )
+
+                self.cache[N] = res
+
+            return self.cache[N]
+    
+    MP = MemoP()
+    i = 1
+    p = 1
+    while p % 1000000:
+        i += 1
+        p = MP.call(i)
+
+    print(i, p)
+    
 #--------------------------------------------------------------------#
 
 import timeit

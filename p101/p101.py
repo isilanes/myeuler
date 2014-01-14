ded8eaa70d6092c107e7ad9bd14a1968d3697cd2
Iñaki Silanes <isilanes@gmail.com>
2014-01-14 11:49:17 +0100

    Solved p101

--- p101/p101.py
+++ p101/p101.py
@@ -3,11 +3,45 @@
 def f0():
     print("--- f0 ---")
 
-    un = []
-    for n in range(10):
-        x = 1 - n + n**2 - n**3 + n**4 - n**5 + n**6 - n**7 + n**8\
-                - n**9 + n**10
-        print n, x
+    import numpy as np
+
+    def pn(coeffs,n):
+        '''
+        Return the value of polynomial with coefficients coeffs and
+        x = n. E.g. coeffs = [1,2,3], n = 7:
+        pn = 1 + 2*7 + 3*7**2 = 162
+        '''
+
+        res = 0
+        for i in range(len(coeffs)):
+            res += coeffs[i] * n**i
+        
+        return res
+
+    # Build terms of exact polynomial:
+    coeffs = [1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1]
+    nc = len(coeffs)
+    un = [ pn(coeffs, n) for n in range(1,nc+1) ]
+
+    # sum of FITs (sFIT) is un[0] (i.e. 1) for OP(1,n), as the first
+    # term will be OP(1,n) = 1 = constant:
+    sFIT = un[0]
+
+    # Sum FITs for OP(2,n) to OP(10,n). Remember OP(N,n) is a N-1 
+    # order polynomial.
+    for N in range(2,nc):
+        # Build a,b to solve with Python's linalg module:
+        a = [ [n**i for i in range(N)] for n in range(1,N+1) ]
+        b = un[:N]
+
+        # X will hold the coefficients such that:
+        # OP(N,n) = \sum_{i=0}^{N-1} X(i)*n**i
+        X = np.linalg.solve(a,b)
+
+        # Once we have X, use it to find FIT: 
+        sFIT += pn(X,N+1)
+
+    print(int(sFIT))
 
 #--------------------------------------------------------------------#
 
@@ -19,10 +53,7 @@ for i in range(1):
     times.append(t.timeit(number=1))
 
 #
-# f0: too slow (~ 1400 s for 10**9 with pypy)
-# f1: too slow (~ 5400 s for 10**9 with pypy)
-# f2: too slow (~  895 s for 10**9 with pypy)
-# f3: ~ 1.3 s (pypy)
+# f0: ~ 83 ms (python2)
 #
 print("\nTimes:\n")
 for i in range(len(times)):

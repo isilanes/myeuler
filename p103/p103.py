edef938789224ad5a2435ef6506a3559e2e0c52a
Iñaki Silanes <isilanes@gmail.com>
2014-01-15 18:05:30 +0100

    update

--- p103/p103.py
+++ p103/p103.py
@@ -14,7 +14,7 @@ def f0(N):
         # All subset pairs:
         for size in range(n,0,-1):
             for combo in it.combinations(S, size):
-                rem = S[:]
+                rem = list(S)
                 for e in combo:
                     rem.remove(e)
                 for i in range(size,len(rem)+1):
@@ -25,13 +25,65 @@ def f0(N):
                         elif sum(combo) == sum(other):
                             return False
                         elif len(combo) > len(other):
-                                return False
+                            return False
 
         return True
 
-    #print is_special([3,4,5,6])
-    #exit()
+    special = [1]
+    for n in range(2,N+1):
+        j = len(special) // 2
+        b = special[j]
+        guess = [b] + [ x + b for x in special ]
+        max_sum = sum(guess)
+        kmax = max_sum - (n-1)*b - (n-1)*(n-2)/2
+        best = guess
+        for combo in it.combinations(range(b+1,kmax+1), n-1):
+            guess = [b] + [ x for x in combo ]
+            if sum(guess) < max_sum and is_special(guess):
+                best = guess
+                max_sum = sum(best)
+        print n, best, ''.join([ str(x) for x  in best ])
+        special = best
 
+#--------------------------------------------------------------------#
+
+def f1(N):
+    print("--- f1 ---")
+
+    import itertools as it
+
+    def is_special(S):
+        '''
+        Return True if S is a valid special set.
+        '''
+
+        # Check that 1..n-1 last elements add up to more than 2..n 
+        # elements combined. If not, exit early.
+        S.sort()
+        n = len(S) // 2
+        for i in range(1,n):
+            if sum(S[-i:]) <= sum(S[:i+1]):
+                return False
+
+        # All subset pairs:
+        for size in range(n,0,-1):
+            for combo in it.combinations(S, size):
+                rem = list(S)
+                for e in combo:
+                    rem.remove(e)
+                for i in range(size,len(rem)+1):
+                    for other in it.combinations(rem, i):
+                        if sum(combo) > sum(other):
+                            if len(combo) < len(other):
+                                #return False
+                                pass
+                        if sum(combo) == sum(other):
+                            return False
+                        elif len(combo) > len(other):
+                            return False
+
+        return True
+    
     special = [1]
     for n in range(2,N+1):
         j = len(special) // 2
@@ -53,8 +105,8 @@ def f0(N):
 import timeit
 
 times = []
-for i in range(1):
-    t = timeit.Timer('f{0}(7)'.format(i), "from __main__ import f{0}".format(i))
+for i in range(2):
+    t = timeit.Timer('f{0}(5)'.format(i), "from __main__ import f{0}".format(i))
     times.append(t.timeit(number=1))
 
 #

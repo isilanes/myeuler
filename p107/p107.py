19c0bdb406571931b4ddbe5c349f6620db9c0916
isilanes <isilanes@gmail.com>
2014-01-18 22:22:49 +0100

    Solved p107

--- p107/p107.py
+++ p107/p107.py
@@ -1,51 +1,101 @@
 #--------------------------------------------------------------------#
 
-def f0(N):
+def f0():
+    '''
+    http://en.wikipedia.org/wiki/Prim's_algorithm.
+    '''
     print("--- f0 ---")
 
-    import numpy as np
+    class Vertex(object):
 
-    # Grid with all network info:
-    G = np.zeros((40,40), dtype=np.int)
+        def __init__(self):
+            self.i = None
+            self.edges = []
 
+    class Edge(object):
+
+        def __init__(self):
+            self.w = None
+            self.src = None
+            self.dest = None
+
+    class Graph(object):
+
+        def __init__(self):
+            self.vertices = {}
+            self.edges = []
+        
     # Read grid info:
+    G = Graph()
     with open("network.txt") as f:
-        iline = 0
+        i = 0
         for line in f:
-            aline = []
             a = line.strip().split(',')
-            for e in a:
-                if e == '-':
-                    aline.append(0)
-                else:
-                    aline.append(int(e))
-            G[iline,:] = aline[:]
-            iline += 1
+            for j in range(i+1,len(a)): # only read upper right triangle
+                if a[j] != '-':
+                    # Add i -> j edge:
+                    E = Edge()
+                    E.w = int(a[j])
+                    E.src = i
+                    E.dest = j
+                    G.edges.append(E)
 
-    s0 = sum(sum(G))
+                    # Add vertex i:
+                    if not i in G.vertices:
+                        V = Vertex()
+                        V.i = i
+                        G.vertices[i] = V
+                    G.vertices[i].edges.append(E)
 
-    # Keep looping over check on upper right cells until complete
-    # cycle without removal. A cell is removed IFF its value is max
-    # in its column AND max in its row AND removing it doesn't leave
-    # the column NOR row empty.
-    removal = True
-    while removal:
-        removal = False
-        for icol in range(1,40):
-            for irow in range(40):
-                # Max and second max in row:
-                max1, max = sorted(G[irow,:])[-2:]
-                if G[irow,icol] == max and max1 > 0:
-                    # Then check column:
-                    max1, max = sorted(G[:,icol])[-2:]
-                    if G[irow,icol] == max and max1 > 0:
-                        G[irow,icol] = 0
-                        G[icol,irow] = 0
-                        removal = True
+                    # Add j -> i edge:
+                    E = Edge()
+                    E.w = int(a[j])
+                    E.src = j
+                    E.dest = i
+                    G.edges.append(E)
 
-    s1 = sum(sum(G))
+                    # Add vertex j:
+                    if not j in G.vertices:
+                        V = Vertex()
+                        V.i = j
+                        G.vertices[j] = V
+                    G.vertices[j].edges.append(E)
+            i += 1
 
-    print (s0 - s1) / 2
+    # Initial sum:
+    s0 = 0
+    for e in G.edges:
+        s0 += e.w
+    s0 = s0/2 # avoid duplication
+
+    # Prim's algorithm:
+    Vnew = [0] # selected vertices, pick first at "random"
+    Enew = [] # selected edges
+    while len(Vnew) < len(G.vertices):
+        # Pick all edges coming out of vertices already in Vnew,
+        # but pointing to a vertex still out of Vnew:
+        outwards = None
+        min_val = 9999
+        for i in Vnew:
+            v = G.vertices[i]
+            for e in v.edges:
+                if not e.dest in Vnew:
+                    if e.w < min_val:
+                        outwards = e
+                        min_val = e.w
+
+        # Add chosen edge to list of edges (Enew), and chosen vertex
+        # to list of vertices (Vnew):
+        Vnew.append(outwards.dest)
+        Enew.append(outwards)
+
+    # Final sum:
+    tot = 0
+    for e in Enew:
+        tot += e.w
+
+    # "Saved":
+    print(s0 - tot)
 
 #--------------------------------------------------------------------#
 
@@ -53,11 +103,11 @@ import timeit
 
 times = []
 for i in range(1):
-    t = timeit.Timer('f{0}(7)'.format(i), "from __main__ import f{0}".format(i))
+    t = timeit.Timer('f{0}()'.format(i), "from __main__ import f{0}".format(i))
     times.append(t.timeit(number=1))
 
 #
-# f0:
+# f0: ~ 9.4 ms (python2)
 #
 print("\nTimes:\n")
 for i in range(len(times)):

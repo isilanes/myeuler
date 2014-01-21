b4531a0a491269cd786c5dabb9c44519a8cc87a5
Iñaki Silanes <isilanes@gmail.com>
2014-01-21 18:05:55 +0100

    update p110

--- p110/p110.py
+++ p110/p110.py
@@ -3,64 +3,16 @@
 def f0():
     print("--- f0 ---")
 
-    import math
     import itertools as it
 
-    def ncombos2(n):
-        '''
-        Return amount of valid (x,y) combos for a given n.
-        '''
-
-        combos = []
-        for N in range(1,n):
-            if N*N > n:
-                break
-            for k in range(1,n):
-                if not n % (k*N):
-                    M = n / (k*N)
-                    x = k*M*(M+N)
-                    y = k*N*(M+N)
-                    c = [x,y]
-                    c.sort()
-                    if not c in combos:
-                        combos.append(c)
-
-        return len(combos)
-    
-    # Find proper divisors of all numbers up to max, using a sieve:
-    nmax = 20480
-    propers = {}
-    for i in range(1,nmax+1):
-        p = i*2
-        while p < nmax+1:
-            try:
-                propers[p].append(i)
-            except:
-                propers[p] = [i]
-            p += i
-
-    def ncombos(z, propsz):
+    def ncombos(pfactors):
         '''
         Return amount of valid (x,y) combos for a given z.
         '''
 
-        combos = []
-        for k in propsz:
-            mn = z / k
-            propsmn = propsz[:]
-            if k != 1:
-                propsmn.remove(k)
-            for m in propsmn:
-                if m*m > mn:
-                    break
-                n = mn/m
-                x = k * m * (m+n)
-                y = k * n * (m+n)
-                if not [x,y] in combos:
-                    combos.append([x,y])
+        pz = propers(pfactors+pfactors)
+        return len(pz) + 1
 
-        return len(combos)
-    
     def primes(n):
         ps = []
         for i in range(2,n):
@@ -76,29 +28,28 @@ def f0():
         proper divisors.
         '''
 
-        props = [1]
+        props = { 1: (1,) }
         for n in range(1,len(pfactors)):
             for combo in it.combinations(pfactors, n):
                 fac = 1
                 for e in combo:
                     fac = fac*e
-                props.append(fac)
+                props[fac] = True
 
-        return list(set(props))
+        return props.keys()
 
-    ps = [2,2,3,3,5,5,7,7,11,11,13,13,17,17]
-    ps = [2,3]
-    max_nc = 0
-    for N in range(2,3):
-        for combo in it.combinations(ps, N):
-            n = 1
-            for c in combo:
-                n = n*c
-            props = propers(combo)
-            nc = ncombos(n, props)
-            if nc > max_nc:
-                max_nc = nc
-                print n, nc, combo
+    combo = [2,3,5,7,11,13,17,19,23]               #                  9842
+    combo = [2,2,3,5,7,11,13,17,19,23]             #                 16403
+    combo = [2,2,3,3,5,7,11,13,17,19,23]           #    1338557220,  27338
+    combo = [2,2,3,3,5,5,7,11,13,17,19,23]         #    6692786100,  45563
+    combo = [2,2,3,3,5,5,7,7,11,13,17,19,23]       #   46849502700,  75938
+    combo = [2,2,3,3,5,5,7,7,11,11,13,17,19,23]    #  515344529700, 126563
+    combo = [2,2,3,3,5,5,7,7,11,11,13,13,17,19,23] # 
+    d = (ncombos(combo) + 1)/2
+    n = 1
+    for e in combo:
+        n = n * e
+    print n, combo, d, d/4000*1000.0
 
 #--------------------------------------------------------------------#
 

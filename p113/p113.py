9e3345cdfa2a3df3834a52bbc974e93608ba8da6
isilanes <isilanes@gmail.com>
2014-01-24 00:10:38 +0100

    go on with p113

--- p113/p113.py
+++ p113/p113.py
@@ -3,11 +3,41 @@
 def f0():
     print("--- f0 ---")
 
-    # Increasings:
-    for i in range(1,10):
-        for j in range(i,10):
-            for k in range(j,10):
-                print i,j,k
+    # --- Increasings numbers --- #
+
+    # Digits go from 1st to 100th. For increasing
+    # numbers ith digit must be equal to or higher than
+    # (i-1)th digit.
+
+    # When 99th digit is i, sum of all possible 100th
+    # digit cases is s[i]:
+    s = [ 10 - x for x in range(10) ]
+
+    # There are 98 previous digits for which the process
+    # must be repeated:
+    for j in range(4):
+        for i in range(10):
+            s[i] = sum(s[i:])
+
+    nincreasing = sum(s)
+    nincreasing -= 1 # do not count zero as "increasing" (or anything)
+
+    print nincreasing
+
+    # --- Decreasing numbers --- #
+
+    # Recall numbers with M leading zeros can still be decreasing
+    # numbers (even if (M+1)th digit is larger than zero (Mth),
+    # because they are actually (100-M)-digit numbers. So to find
+    # out the amount of decreasing numbers we must do otherwise.
+    for ndigs in range(2,101):
+        pass
+
+    # Also, in general, all N-digit numbers, N=1 to 100, in
+    # which all digits are equal have been counted twice:
+    nonbouncy -= 6*9
+
+    print(nonbouncy)
 
 #--------------------------------------------------------------------#
 
@@ -19,7 +49,7 @@ for i in range(1):
     times.append(t.timeit(number=1))
 
 #
-# f0: ~ 1.6 s (pypy)
+# f0:
 #
 print("\nTimes:\n")
 for i in range(len(times)):

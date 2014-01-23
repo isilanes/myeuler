00291200e734172292f6f0a3e040995e2c9c3830
Iñaki Silanes <isilanes@gmail.com>
2014-01-23 18:03:37 +0100

    Start with p113

---
+++ p113/p113.py
@@ -0,0 +1,26 @@
+#--------------------------------------------------------------------#
+
+def f0():
+    print("--- f0 ---")
+
+    # Increasings:
+    for i in range(1,10):
+        for j in range(i,10):
+            for k in range(j,10):
+                print i,j,k
+
+#--------------------------------------------------------------------#
+
+import timeit
+
+times = []
+for i in range(1):
+    t = timeit.Timer('f{0}()'.format(i), "from __main__ import f{0}".format(i))
+    times.append(t.timeit(number=1))
+
+#
+# f0: ~ 1.6 s (pypy)
+#
+print("\nTimes:\n")
+for i in range(len(times)):
+    print('t{0} = {1:.2f} ms'.format(i, times[i]*1000))

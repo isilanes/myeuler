0c6ee8ceb34ff21209443617fd833cdf3493a6b1
Iñaki Silanes <isilanes@gmail.com>
2014-01-24 11:50:42 +0100

    Solved 113

--- p113/p113.py
+++ p113/p113.py
@@ -3,6 +3,8 @@
 def f0():
     print("--- f0 ---")
 
+    ndigits = 100
+
     # --- Increasings numbers --- #
 
     # Digits go from 1st to 100th. For increasing
@@ -15,14 +17,12 @@ def f0():
 
     # There are 98 previous digits for which the process
     # must be repeated:
-    for j in range(4):
+    for j in range(ndigits-2):
         for i in range(10):
             s[i] = sum(s[i:])
 
-    nincreasing = sum(s)
-    nincreasing -= 1 # do not count zero as "increasing" (or anything)
-
-    print nincreasing
+    # Do not count zero (subtract one):
+    nincreasing = sum(s) - 1
 
     # --- Decreasing numbers --- #
 
@@ -30,13 +30,29 @@ def f0():
     # numbers (even if (M+1)th digit is larger than zero (Mth),
     # because they are actually (100-M)-digit numbers. So to find
     # out the amount of decreasing numbers we must do otherwise.
-    for ndigs in range(2,101):
-        pass
 
-    # Also, in general, all N-digit numbers, N=1 to 100, in
-    # which all digits are equal have been counted twice:
-    nonbouncy -= 6*9
+    # For 1 digit:
+    ndecreasing = 9 # 10 minus the zero
 
+    # For 2 digits:
+    s = [ x for x in range(1,11) ]
+    ndecreasing += sum(s) - 1 # minus the zero
+
+    # For 3+ digits:
+    for j in range(2,ndigits):
+        for i in range(9,-1,-1):
+            s[i] = sum(s[:i+1])
+        ndecreasing += sum(s) - 1 # minus the zero
+
+    # Amount of non-bouncy numbers is amount of increasing plus
+    # amount of decreasing (plus reduntands, see below):
+    nonbouncy = nincreasing + ndecreasing
+
+    # All j-digit numbers, j=1 to j=ndigits, in which all digits 
+    # are equal have been counted twice (remove them once):
+    nonbouncy -= ndigits*9
+
+    # Result:
     print(nonbouncy)
 
 #--------------------------------------------------------------------#
@@ -49,7 +65,7 @@ for i in range(1):
     times.append(t.timeit(number=1))
 
 #
-# f0:
+# f0: ~ 1.1 ms (python2)
 #
 print("\nTimes:\n")
 for i in range(len(times)):

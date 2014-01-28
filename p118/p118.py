b6a6c4431781cb434a449f6fb5aa6f9e3644c1f6
isilanes <isilanes@gmail.com>
2014-01-28 23:16:04 +0100

    update p118

--- p118/p118.py
+++ p118/p118.py
@@ -5,9 +5,62 @@ def f0():
 
     import itertools as it
 
+    def find_prime(combo):
+        '''
+        Given string of digits "combo", find a prime by succesively
+        concatenating first N digits for N=1,2,3... Return the
+        first prime it finds, False if none found.
+        '''
+
+        num = ''
+        for c in combo:
+            num += str(c)
+            if isprime(int(num)):
+                return int(num)
+
+        return False
+
+    def isprime(num):
+        '''
+        Returns True if num is prime, False otherwise.
+        '''
+
+        if num == 1:
+            return False
+
+        if num in [2,3,5,7]:
+            return True
+
+        if num % 10 in [0,2,4,5,6,8]:
+            return False
+
+        i = 3
+        while i*i < num:
+            if not num % i:
+                return False
+            i += 2
+
+        return True
+
     tot = 0
+    #for combo in it.permutations([2,5,4,7,8,9,6,3,1], 9):
     for combo in it.permutations([1,2,3,4,5,6,7,8,9], 9):
-        tot += 1
+        if combo[-1] in [0,2,4,5,6,8]:
+            continue
+        last = 0 # 
+        while end < 9:
+            primes = []
+            while combo:
+                res = find_prime(combo)
+                if res:
+                    primes.append(res)
+                    i = len(str(res))
+                    combo = combo[i:]
+                    if not combo: # success! yay!
+                        #print primes
+                        tot += 1
+                else:
+                    break
 
     print(tot)
 

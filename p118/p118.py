412a89c9e9bfb79a819bd304b09b8f5ffd7f2810
Iñaki Silanes <isilanes@gmail.com>
2014-01-30 18:17:56 +0100

    update p118

--- p118/p118.py
+++ p118/p118.py
@@ -27,9 +27,15 @@ def f0():
 
         return True
 
+    def s2pos(s, exclu):
+        pos = exclu[s[0]]
+        for dig in s[1:]:
+            pos = [ x for x in pos if x in exclu[dig] ]
+        return pos
+
     # dictionary of digit -> list of primes not containing digit
     exclu = {
-            '1': [2,3,5,7,],
+            '1': [2,3,5,7],
             '2': [3,5,7],
             '3': [2,5,7],
             '4': [2,3,5,7],
@@ -42,39 +48,68 @@ def f0():
 
     # Harvest all primes that don't have repeated digits, 
     # and classify them into exclu above:
+    primes = [2,3,5,7]
     for ndig in range(2,8):
-        for combo in it.combinations('123456789', ndig):
+        for combo in it.permutations('123456789', ndig):
             num = ''.join(combo)
             num = int(num)
             if isprime(num):
+                primes.append(num)
                 for si in '123456789':
                     if not si in combo:
                         exclu[si].append(num)
-    num = 631
-    s = str(num)
-    pos = exclu[s[0]]
-    for dig in s[1:]:
-        pos = [ x for x in pos if x in exclu[dig] ]
-    print num, pos
-    for num2 in pos:
-        s = str(num) + str(num2)
-        pos = exclu[s[0]]
-        for dig in s[1:]:
-            pos = [ x for x in pos if x in exclu[dig] ]
-        print num, num2, pos
-        if not pos:
-            if len(s) == 9:
-                eureka
-            else:
-                this num, num2 not in solution
 
-    return
-    for k,v in exclu.items():
-        for num in v:
-            pos = exclu[str(num)[0]]
-            for dig in str(num)[1:]:
-                pos = [ x for x in pos if x in exclu[dig] ]
-        return
+    valids = []
+    for num1 in primes:
+        s = str(num1)
+        pos = s2pos(s, exclu)
+
+        for num2 in pos:
+            s = str(num1) + str(num2)
+            pos = s2pos(s, exclu)
+            if len(s) == 9:
+                c = sorted([num1, num2])
+                valids.append(tuple(c))
+
+            for num3 in pos:
+                s = str(num1) + str(num2) + str(num3)
+                pos = s2pos(s, exclu)
+                if len(s) == 9:
+                    c = sorted([num1, num2, num3])
+                    valids.append(tuple(c))
+
+                for num4 in pos:
+                    s = str(num1) + str(num2) + str(num3) + str(num4)
+                    pos = s2pos(s, exclu)
+                    if len(s) == 9:
+                        c = sorted([num1, num2, num3, num4])
+                        valids.append(tuple(c))
+
+                    for num5 in pos:
+                        s = str(num1) + str(num2) + str(num3) + str(num4) + str(num5)
+                        pos = s2pos(s, exclu)
+                        if len(s) == 9:
+                            c = sorted([num1, num2, num3, num4, num5])
+                            valids.append(tuple(c))
+
+                        for num6 in pos:
+                            s = str(num1) + str(num2) + str(num3) + str(num4) + str(num5) + str(num6)
+                            pos = s2pos(s, exclu)
+                            if len(s) == 9:
+                                c = sorted([num1, num2, num3, num4, num5, num6])
+                                valids.append(tuple(c))
+
+                            for num7 in pos:
+                                s = str(num1) + str(num2) + str(num3) + str(num4) + str(num5) + str(num6)+str(num7)
+                                pos = s2pos(s, exclu)
+                                if len(s) == 9:
+                                    c = sorted([num1, num2, num3, num4, num5, num6, num7])
+                                    valids.append(tuple(c))
+
+    valids = set(valids)
+    for valid in valids:
+        print valid
+    print(len(valids))
 
 #--------------------------------------------------------------------#
 

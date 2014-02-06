af41a6a16c3bc1eb88c6eb01da08f8fb83c56a5a
Iñaki Silanes <isilanes@gmail.com>
2014-02-06 18:06:15 +0100

    start with p122

---
+++ p122/p122.py
@@ -0,0 +1,20 @@
+#--------------------------------------------------------------------#
+
+def f0():
+    print("--- f0 ---")
+
+#--------------------------------------------------------------------#
+
+import timeit
+
+times = []
+for i in range(1):
+    t = timeit.Timer('f{0}()'.format(i), "from __main__ import f{0}".format(i))
+    times.append(t.timeit(number=1))
+
+#
+# f0: ~ 10 ms (python3)
+#
+print("\nTimes:\n")
+for i in range(len(times)):
+    print('t{0} = {1:.2f} ms'.format(i, times[i]*1000))

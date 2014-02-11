b512ea3fe2c140e70681acabb396100cc0dcb8b5
isilanes <isilanes@gmail.com>
2014-02-11 23:36:31 +0100

    more advances with p126

--- p126/p126.py
+++ p126/p126.py
@@ -73,7 +73,7 @@ def f0():
         return res
 
     # Maximum cubes per single layer:
-    nmax = 500
+    nmax = 2000
 
     # Max dimension for A:
     Amax = nmax / 4
@@ -95,11 +95,12 @@ def f0():
                         cub[e] += 1
                     except:
                         cub[e] = 1
-
-    print cub[22]
-    print cub[46]
-    print cub[78]
-    print cub[118]
+                    if cub[e] > 999:
+                        print e, cub[e]
+    print cub[22]  # 2
+    print cub[46]  # 4
+    print cub[78]  # 5
+    print cub[118] # 8
 
     for i in sorted(cub.keys()):
         if i in cub:
@@ -151,34 +152,147 @@ def f2():
     def nlayers(A,B,C,n):
         '''
         '''
-
-        # Face AB:
-        nAB = [A*B]
-        for ilayer in range(1,n):
-            ni = nAB[-1] + (A + B) + 4*(ilayer-1)
-            nAB.append(ni)
-
-        # Face AC:
-        nAC = [A*C,0,0]
-
-        # Face BC:
-        nBC = [B*C,0,0]
+        
+        v0 = 2*A*B + 2*A*C + 2*B*C
 
         res = []
-        for i,j,k in zip(nAB,nAC,nBC):
-            v = 2*(i+j+k)
+        for i in range(n):
+            v = v0 + 4*i*(A+B+C) + 8*(i-1)*0
             res.append(v)
 
         return res
 
-    print nlayers(3,2,1,4)
+    print nlayers(3,2,1,4)  # 22, 46, 78, 118
+    print nlayers(5,1,1,1)  # 22
+    print nlayers(5,3,1,1)  # 46
+    print nlayers(7,2,1,1)  # 46
+    print nlayers(11,1,1,1) # 46
+
+#--------------------------------------------------------------------#
+
+def f3():
+    print("--- f3 ---")
+
+    def nlayer(A,B,C,nmax):
+        '''
+        Returns an array of [n1,n2,n3...nj], where ni is the amount of
+        cubes to fill i-th layer on a AxBxC cuboid. The list of layers
+        goes one until layer j, where nj first exceeds nmax.
+        '''
+
+        if not A % 2:
+            A = A / 2
+            dirsA = [(1,0,0)]
+            nmulA = 2
+        else:
+            dirsA = [(1,0,0), (-1,0,0)]
+            nmulA = 1
+
+        if not B % 2:
+            B = B / 2
+            dirsB = [(0,1,0)]
+            nmulB = 2
+        else:
+            dirsB = [(0,1,0), (0,-1,0)]
+            nmulB = 1
+
+        if not C % 2:
+            C = C / 2
+            dirsC = [(0,0,1)]
+            nmulC = 2
+        else:
+            dirsC = [(0,0,1), (0,0,-1)]
+            nmulC = 1
+
+        dirs = dirsA + dirsB + dirsC
+        nmul = nmulA * nmulB * nmulC
+
+        # A dict with (x,y,z) -> True, where (x,y,z) are positions
+        # in space that are filled:
+        occu = {}
+        tmp = {} # tmp copy of occu. tmp varies, occu is fixed:
+
+        # Initial fill of occu:
+        for i in range(A):
+            for j in range(B):
+                for k in range(C):
+                    occu[(i,j,k)] = True
+                    tmp[(i,j,k)] = True
+
+        # Fill adjacents to ABC, and count them (only once!):
+        res = []
+        ind = 0
+
+        to_test = occu.keys()[:]
+        while True:
+            new = []
+            res.append(0)
+            for i,j,k in to_test:
+                for dir in dirs:
+                    adj = (i+dir[0], j+dir[1], k+dir[2])
+                    if not adj in occu:
+                        occu[adj] = True
+                        res[ind] += nmul
+                        new.append(adj)
+
+            to_test = new[:]
+            
+            ind += 1
+            if res[-1] > nmax:
+                break
+
+        return res
+
+    # Maximum cubes per single layer:
+    nmax = 7000
+
+    # Max dimension for A:
+    Amax = nmax / 4
+
+    # Dict with n -> a, where a is amount of layers in various AxBxC
+    # cuboids to have exactly n cubes.
+    cub = {}
+    
+    for A in range(1,Amax+1):
+        for B in range(1,A+1):
+            if 2*A*B + 2*(A+B) > nmax:
+                break
+            for C in range(1,B+1):
+                if 2*A*B + 2*A*C + 2*B*C > nmax:
+                    break
+                nl = nlayer(A,B,C,nmax)
+                for e in nl:
+                    try:
+                        cub[e] += 1
+                    except:
+                        cub[e] = 1
+                    if cub[e] > 999:
+                        print e, cub[e]
+
+    print cub[22]  # 2
+    print cub[46]  # 4
+    print cub[78]  # 5
+    print cub[118] # 8
+    print nlayer(3,2,1,4)  # 22, 46, 78, 118
+    print nlayer(5,1,1,1)  # 22
+    print nlayer(5,3,1,1)  # 46
+    print nlayer(7,2,1,1)  # 46
+    print nlayer(11,1,1,1) # 46
+
+    for i in sorted(cub.keys()):
+        if i in cub:
+            if cub[i] == 100:
+                print ">", i
+            elif cub[i] == 1000:
+                print ">>", i
+                break
 
 #--------------------------------------------------------------------#
 
 import timeit
 
 times = []
-for i in range(2,3):
+for i in [3]:
     t = timeit.Timer('f{0}()'.format(i), "from __main__ import f{0}".format(i))
     times.append(t.timeit(number=1))
 

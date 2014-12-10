0b9feb137ca6abd71147f3ebb73c7cd4f6630666
isilanes <isilanes@gmail.com>
2014-12-10 21:00:35 +0100

    minor advance in p147

--- p147/p147.py
+++ p147/p147.py
@@ -11,6 +11,17 @@ def f0(M,N):
             c = (M - m + 1) * (N - n + 1)
             print m, "x", n, "=", c
 
+    # Oblique rectangles are more complex. Take i = 0, 1, ..., 2*M as horizontal
+    # indices evenly placed at every half-square, and j = 0, 1, ..., 2*N as their
+    # vertical equivalent. Every vertex of an oblique rectangle will lie in some
+    # (i,j) point. We will check each valid top left vertex, and see how many
+    # rectangles it can form by varying the W width and H height of the potential
+    # rectangle, and seeing if the resulting top right, bottom-left and bottom-right
+    # vertices are within the MxN figure (that is, the (i,j) of all vertices are
+    # within (0,0) and (imax, jmax), i.e. (2*M, 2*N).
+    for i in range(2*M):
+        for j in range()
+
 
 #------------------------------------------------------------------------------#
 

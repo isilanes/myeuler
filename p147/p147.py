a5806ddf0f5c07ec3dbfaf8f1242cc906a06ccc2
isilanes <isilanes@gmail.com>
2016-08-04 19:07:55 +0200

    rubbish

--- p147/p147.py
+++ p147/p147.py
@@ -3,7 +3,69 @@ import timeit
 def f0(M,N):
     print("--- f0 ---")
 
-<<<<<<< HEAD
+    def NCA1(T, A, B, i, j):
+        nca = 0
+        r = max(0, B-i)
+        Ap = A - r
+        for k in range(Ap):
+            for l in range(Ap-k):
+                nca += (T - i - k) * (T - j - r - l)
+
+        return nca
+
+    def NCB1(T, A, B, i, j):
+        ncb = 0
+        r = max(0, A-i)
+        q = max(0, B-j)
+        Bp = B - r
+        for k in range(Bp):
+            for l in range(Bp-k):
+                ncb += (T - i - q - k) * (T - j - r - l)
+
+        return ncb
+
+    def NEA1(T ,A):
+        tot = 0
+        for i in range(A):
+            for j in range(A-i):
+                tot += (T - i) * (T - j)
+
+        print(["NEA1", tot])
+        return tot
+
+    def NEB1(T, A, B):
+        tot = 0
+        for i in range(B):
+            r = max(0, A-i)
+            for j in range(B-i):
+                tot += (T - i) * (T - j - r)
+
+        print(["NEB1", tot])
+        return tot
+
+    def NEA2(T,A,B):
+        tot = 0
+        for i in range(A):
+            r = max(0, B-i)
+            for j in range(A-i):
+                tot += (T - i) * (T - j - r)
+                tot -= NCA1(T, A, B, i, j)
+
+        print(["NEA2", tot])
+        return tot
+
+    def NEB2(T,A,B):
+        tot = 0
+        for i in range(B):
+            r = max(0, A-i)
+            for j in range(B-i):
+                q = max(0, B-j)
+                tot += (T - i - q) * (T - j - r)
+                tot -= NCB1(T, A, B, i, j)
+
+        print(["NEB2", tot])
+        return tot
+
     def NHR(M,N):
         """Amount of horizontal rectangles of all sizes, within rectangle of size M*N."""
         
@@ -21,111 +83,14 @@ def f0(M,N):
         A = M - 2
         B = N - 2
 
-        return NHR(T,T) - NEA1(T,A) - NEB1(T,A,B) - NEA2(T,A,B) - NEB2(T,A,B)
-
-    def NEA1(T,A):
-        tot = 0
-        for i in range(A):
-            for j in range(A-i):
-                tot += (T-i) * (T-j)
-
-        print(["NEA1", tot])
-        return tot
-
-    def NEB1(T,A,B):
-        tot = 0
-        for i in range(B):
-            r = max(0, A-i)
-            for j in range(B-i):
-                tot += (T - i) * (T - j - r)
-
-        print(["NEB1", tot])
-        return tot
-
-    def NEA2(T,A,B):
-        tot = 0
-        for i in range(A):
-            r = max(0, B-i)
-            for j in range(A-i):
-                tot += (T - i) * (T - j - r)
-
-        NCA1 = 0
-        tot -= NCA1
-
-        print(["NEA2", tot])
-        return tot
-
-    def NEB2(T,A,B):
-        tot = 0
-        for i in range(A):
-            r = max(0, A-i)
-            for j in range(A-i):
-                q = max(0, B-j)
-                tot += (T -i - q) * (T - j - r)
-
-        NCB1 = 0
-        tot -= NCB1
-
-        print(["NEB2", tot])
-        return tot
+        return NHR(T, T) - NEA1(T, A) - NEB1(T, A, B) - NEA2(T, A, B) - NEB2(T, A, B)
 
 
     # Horizontal rectangles, within rectangle of size M*N
-    print(NHR(M,N))
-    N = NHR(M,N) + NOR(M,N)
+    print(NHR(M, N))
+    N = NHR(M, N) + NOR(M, N)
 
     print(N)
-=======
-
-    class Rectangle(object):
-
-        def __init__(self, M, N):
-            self.M = M # horizonal size
-            self.N = N # vertical size
-
-
-    class Vertex(object):
-
-        def __init__(self, i, j):
-            self.i = i
-            self.j = j
-
-            # All (x,y) dimensions are doubled, i.e., a M*N rectangle is 
-            # assumed to begin at x=0, y=0, and end at x=2M, y=2N.
-            self.x = i + j
-            self.y = 2 + i - j
-
-        def is_within(self, R):
-            '''Determine whether vertex is within the bounds of
-            rectangle R.'''
-
-            if self.x > 2*R.M:
-                return False
-            
-            if self.y < 0 or self.y > 2*R.N:
-                return False
-
-            return True
-
-
-    # Horizontal rectangles of size m*n, within rectangle of size M*N
-    for m in range(1,M+1):
-        for n in range(1,N+1):
-            c = (M - m + 1) * (N - n + 1)
-            print m, "x", n, "=", c
->>>>>>> 0ec45c58907f6fa0b22db306a037ee76d93193d9
-
-    # Oblique rectangles are more complex. Take i = 0, 1, ..., 2*M as horizontal
-    # indices evenly placed at every half-square, and j = 0, 1, ..., 2*N as their
-    # vertical equivalent. Every vertex of an oblique rectangle will lie in some
-    # (i,j) point. We will check each valid top left vertex, and see how many
-    # rectangles it can form by varying the W width and H height of the potential
-    # rectangle, and seeing if the resulting top right, bottom-left and bottom-right
-    # vertices are within the MxN figure (that is, the (i,j) of all vertices are
-    # within (0,0) and (imax, jmax), i.e. (2*M, 2*N).
-    R = Rectangle(M, N)
-    TL = Vertex(0,0)
-    print TL.is_within(R)
 
 
 #------------------------------------------------------------------------------#

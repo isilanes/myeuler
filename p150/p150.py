bb8574c82e152ddee30896a7e504ffb7e0f87399
Iñaki Silanes <isilanes@gmail.com>
2016-08-31 12:45:13 +0200

    p150: f1, with memoized f0. Too-memory hungry

--- p150/p150.py
+++ p150/p150.py
@@ -1,224 +1,27 @@
 # -*- coding=utf-8 -*-
 
 import timeit
-
-def f5(n, disp=True):
-
-    if disp:
-        print("--- f5 ---")
-
-    def memoize(f):
-        cache = {}
-        def helper(x):
-            if x not in cache:
-                cache[x] = f(x)
-
-            return cache[x]
-
-
-        return helper
-
-    @memoize
-    def s(k):
-        if k < 56:
-            return (100003 - 200003*k + 300007*k**3) % 1000000 - 500000
-        else:
-            return (s(k-24) + s(k-55) + 1000000) % 1000000 - 500000
-
-    def max_sum(arr):
-        """Take list "arr" and return largest sum of consecutive elements."""
-
-        # "compact" to all-positive and all-negative "islands":
-        compacted = [arr[0]]
-
-        for e in arr[1:]:
-            if e*compacted[-1] > 0: # equal sign
-                compacted[-1] += e
-            else:
-                compacted.append(e)
-
-        # Remove first one, if negative, so we start from positive:
-        if compacted[0] < 0:
-            compacted = compacted[1:]
-
-        if not compacted: # then ALL were negative
-            return 0
-
-        mx = compacted[0]
-        cum = mx
-        m = len(compacted)
-
-        for i in range(2,m,2):
-            ci = compacted[i]
-            cim1 = compacted[i-1]
-            cum1 = cum + cim1
-            if cum1 > 0:
-                cum = cum1 + ci
-            else:
-                cum = ci
-            mx = max(mx, cum, ci)
-
-        mx = max(mx, cum)
-
-        return mx
-
-    def max_horizontal(a):
-        """Generate all horizontal rows, and return max sum of any."""
-
-        ms = 0
-        for row in a:
-            ret = max_sum(row)
-            if ret > ms:
-                ms = ret
-
-        return ms
-
-    def max_vertical(a,n):
-        """Generate all vertical columns, and return max sum of any."""
-
-        ms = 0
-        for j in range(n):
-            col = [ a[i][j] for i in range(n) ]
-            ret = max_sum(col)
-            if ret > ms:
-                ms = ret
-
-        return ms
-
-    def max_bltr_diagonals(a,n):
-        """Generate all botton-left to top-right type lines, and return max sum of any."""
-
-        ms = 0
-        for j in range(n):
-            #diag = [ a[k,j-k] for k in range(j+1) ]
-            diag = [ a[k][j-k] for k in range(j+1) ]
-            ret = max_sum(diag)
-            if ret > ms:
-                ms = ret
-
-        for i in range(1,n):
-            #diag = [ a[k,i-k+n-1] for k in range(i,n) ]
-            diag = [ a[k][i-k+n-1] for k in range(i,n) ]
-            ret = max_sum(diag)
-            if ret > ms:
-                ms = ret
-
-        return ms
-
-    def max_tlbr_diagonals(a,n):
-        """Generate all top-left to bottom-right type lines, and return max sum of any."""
-
-        ms = 0
-        for i in range(n):
-            #diag = [ a[k,k-i] for k in range(i,n) ]
-            diag = [ a[k][k-i] for k in range(i,n) ]
-            ret = max_sum(diag)
-            if ret > ms:
-                ms = ret
-
-        for j in range(1,n):
-            #diag = [ a[k-j,k] for k in range(j,n) ]
-            diag = [ a[k-j][k] for k in range(j,n) ]
-            ret = max_sum(diag)
-            if ret > ms:
-                ms = ret
-
-        return ms
-
-
-    # Check it works correctly:
-    assert s(10) == -393027
-    assert s(100) == 86613
-
-    # Generate board:
-    #a = [ s(i) for i in range(1,n*n+1) ]
-    #a = np.array(a).reshape((n,n))
-    a = []
-    for i in range(n):
-        a.append([])
-        for j in range(n):
-            ij = i*n + j + 1
-            a[i].append(s(ij))
-
-    # Check horizontal rows (will be max so far):
-    ms = max_horizontal(a)
-
-    # Check vertical columns:
-    ret = max_vertical(a,n)
-    if ret > ms:
-        ms = ret
-
-    # Check BL-TR diagonals:
-    ret = max_bltr_diagonals(a,n)
-    if ret > ms:
-        ms = ret
-
-    # Check TL-BR diagonals:
-    ret = max_tlbr_diagonals(a,n)
-    if ret > ms:
-        ms = ret
-
-    if disp:
-        print(ms)
-
-    return ms
+from datetime import datetime
 
 def f0(n, disp=True):
 
     if disp:
         print("--- f0 ---")
 
-    def memoize(f):
-        cache = {}
-        def helper(x):
-            if x not in cache:
-                cache[x] = f(x)
+    def build_s(n):
+        """Return s(k) for k in range(1,N+1), as array.
+        N = n*(n+1)/2, that is, "n" is the amount of lines,
+        and N is the amount of numbers.
+        Technically, s(k) would be s[k-1] here: s(1) is the 
+        first (zero) element of the array this function returns.
+        """
+        s = []
+        t = 0
+        for k in range(n*(n+1)//2):
+            t = (615949*t + 797807) % 1048576 # 2**20 = 1048576
+            s.append(t - 524288) # 2**19 = 524288
 
-            return cache[x]
-
-
-        return helper
-
-    @memoize
-    def s(k):
-        return [15, -14, -7, 20, -13, -5, -3, 8, 23, -26, 1, -4, -5, -18, 5, -16, 31, 2, 9, 28, 3][k]
-
-    def max_sum(arr):
-        """Take list "arr" and return largest sum of consecutive elements."""
-
-        # "compact" to all-positive and all-negative "islands":
-        compacted = [arr[0]]
-
-        for e in arr[1:]:
-            if e*compacted[-1] > 0: # equal sign
-                compacted[-1] += e
-            else:
-                compacted.append(e)
-
-        # Remove first one, if negative, so we start from positive:
-        if compacted[0] < 0:
-            compacted = compacted[1:]
-
-        if not compacted: # then ALL were negative
-            return 0
-
-        mx = compacted[0]
-        cum = mx
-        m = len(compacted)
-
-        for i in range(2,m,2):
-            ci = compacted[i]
-            cim1 = compacted[i-1]
-            cum1 = cum + cim1
-            if cum1 > 0:
-                cum = cum1 + ci
-            else:
-                cum = ci
-            mx = max(mx, cum, ci)
-
-        mx = max(mx, cum)
-
-        return mx
+        return s
 
     def subt(triang,i,j,k):
         if k == 0:
@@ -228,22 +31,101 @@ def f0(n, disp=True):
 
 
     # Build triangle:
+    s = build_s(n)
+
     k = 0
     triang = []
     for i in range(n):
         triang.append([])
         for j in range(i+1):
-            triang[-1].append(s(k))
+            triang[-1].append(s[k])
             k += 1
 
     # Find the sums of all subtriangles:
     ms = 0
+    start = datetime.now()
     for i in range(n):
+        now = datetime.now()
         for j in range(i+1):
             for k in range(n-i):
                 ret = subt(triang,i,j,k)
                 if ret < ms:
                     ms = ret
+        end = datetime.now()
+        dt = (end - now).total_seconds()
+        dt0 = (end - start).total_seconds()
+
+    if disp:
+        print(ms)
+
+    return ms
+
+def f1(n, disp=True):
+    """Same as f0, with memoization.
+    Works, but uses INSANE amounts of memory, plus still too slow."""
+
+    if disp:
+        print("--- f1 ---")
+
+    def memoize(f):
+        cache = {}
+        def helper(tr,i,j,k):
+            if (i,j,k) not in cache:
+                cache[(i,j,k)] = f(tr,i,j,k)
+
+            return cache[(i,j,k)]
+
+
+        return helper
+
+    def build_s(n):
+        """Return s(k) for k in range(1,N+1), as array.
+        N = n*(n+1)/2, that is, "n" is the amount of lines,
+        and N is the amount of numbers.
+        Technically, s(k) would be s[k-1] here: s(1) is the 
+        first (zero) element of the array this function returns.
+        """
+        s = []
+        t = 0
+        for k in range(n*(n+1)//2):
+            t = (615949*t + 797807) % 1048576 # 2**20 = 1048576
+            s.append(t - 524288) # 2**19 = 524288
+
+        return s
+
+    @memoize
+    def subt(triang,i,j,k):
+        if k == 0:
+            return triang[i][j]
+        else:
+            return subt(triang,i,j,k-1) + sum(triang[i+k][j:j+k+1])
+
+
+    # Build triangle:
+    s = build_s(n)
+
+    k = 0
+    triang = []
+    for i in range(n):
+        triang.append([])
+        for j in range(i+1):
+            triang[-1].append(s[k])
+            k += 1
+
+    # Find the sums of all subtriangles:
+    ms = 0
+    start = datetime.now()
+    for i in range(n):
+        now = datetime.now()
+        for j in range(i+1):
+            for k in range(n-i):
+                ret = subt(triang,i,j,k)
+                if ret < ms:
+                    ms = ret
+        end = datetime.now()
+        dt = (end - now).total_seconds()
+        dt0 = (end - start).total_seconds()
+        #print(i, ms, 'dt=', dt, 'total:', dt0)
 
     if disp:
         print(ms)
@@ -255,19 +137,32 @@ def f0(n, disp=True):
 
 if __name__ == "__main__":
     times = []
-    for i in [0]:
-        t = timeit.Timer('f{0}(6)'.format(i), "from __main__ import f{0}".format(i))
+    for i in [0,1]:
+        t = timeit.Timer('f{0}(200)'.format(i), "from __main__ import f{0}".format(i))
         times.append([i, t.timeit(number=1)])
 
-    # Python 3.5.2 times
+    # Python 3.4.3 times (Burns)
     #
-    #    n    res(n)  function  time (ms)
-    #  100   8642499        f0      ~1200
-    #                       f1      ~1150
+    #    n     res(n)  function  time (ms)
+    #
+    #    2    -153582        f0        0.2
+    #                        f1        0.1
+    #
+    #   10   -1495491        f0        0.4
+    #                        f1        0.4
+    #
+    #  100  -38951741        f0       3600
+    #                        f1        350
+    #
+    #  200  -87957828        f0      81500
+    #                        f1       3300
 
-    # PyPy 5.1.1 @ Python 2.7.10 times
+    # PyPy 5.1.1 @ Python 2.7.10 times (Skinner)
     #
-    # 2000  52852124        f5       ~950
+    #    n     res(n)  function  time (ms)
+    #
+    #  200  -87957828        f0      24400
+    #                        f1       3100
 
     print("\nTimes:\n")
     for i,t in times:

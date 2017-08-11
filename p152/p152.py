74dfecf639ecf53ba00e6369c4e3349d4d939361
Iñaki Silanes <isilanes@gmail.com>
2017-08-11 14:27:18 +0200

    p152: make f0 work (veeery slowly) and start a better f1

--- p152/p152.py
+++ p152/p152.py
@@ -1,6 +1,6 @@
 # Standard libs:
 import sys
-import random
+import math
 sys.path.append("..")
 
 # Out libs:
@@ -12,14 +12,100 @@ class p152(core.FunctionSet):
 
     # Solutions:
     def f0(self, n):
+        """Slow solution. I thought it was smart, but it appears to be naïve."""
+
+        def deintegerize(number_list):
+            """From numbers (lcm/n)**2, return list of n values."""
+
+            return [int(lcm/math.sqrt(val)) for val in number_list]
+
+        def groups(target, number_list):
+            """Return list of all subsets of integers in 'number_list' that add to 'target'."""
+
+            if not number_list: # empty list
+                return []
+
+            if sum(number_list) == target:
+                return [number_list]
+
+            ret = []
+            for i, number in enumerate(number_list):
+                if target == number:
+                    ret.append([number])
+                    continue
+
+                subtarget = target - number
+                subnumber_list = number_list[i+1:]
+                for group in groups(subtarget, subnumber_list):
+                    print("DEBUG40", deintegerize(subgroup))
+                    if group:
+                        subgroup = [number]
+                        subgroup.extend(group)
+                        print("DEBUG42", deintegerize(subgroup))
+                        ret.append(subgroup)
+
+            return ret
+
+
         lcm = core.lcm_of_many(range(2, n))
-        elements = [(lcm // e)**2 for e in range(2, n)]
-        DEST = lcm // 2
+        elements = [(lcm // e)**2 for e in range(2, n+1)]
+        TARGET = lcm**2 // 2
 
-        for e in elements:
-            print(e)
+        print(TARGET)
+        print(deintegerize(elements))
+        g = groups(TARGET, elements)
+        for group in g:
+            print(group)
+
+    def f1(self, n):
+        """Like f0, but sorted in reverse, so some 'clever' assumption can be made.
+        number_list must be ordered from smallest to largest.
+        """
+        def deintegerize(number_list):
+            """From numbers (lcm/n)**2, return list of n values."""
+
+            return [int(lcm/math.sqrt(val)) for val in number_list]
+
+        def groups(target, number_list):
+            """Return list of all subsets of integers in 'number_list' that add to 'target'."""
+
+            if not number_list: # empty list
+                return []
+
+            if number_list[0] > target:
+                return []
+
+            if sum(number_list) == target:
+                return [number_list]
+
+            ret = []
+            for i, number in enumerate(number_list):
+                if target == number:
+                    ret.append([number])
+                    continue
+
+                subtarget = target - number
+                subnumber_list = number_list[i+1:]
+                for group in groups(subtarget, subnumber_list):
+                    print("DEBUG40", deintegerize(subgroup))
+                    if group:
+                        subgroup = [number]
+                        subgroup.extend(group)
+                        print("DEBUG42", deintegerize(subgroup))
+                        ret.append(subgroup)
+
+            return ret
 
 
+        lcm = core.lcm_of_many(range(2, n))
+        elements = [(lcm // e)**2 for e in range(n, 1, -1)]
+        TARGET = lcm**2 // 2
+
+        print(TARGET)
+        print(deintegerize(elements))
+        g = groups(TARGET, elements)
+        for group in g:
+            print(group)
 
 
 # Main code:
@@ -31,5 +117,5 @@ if __name__ == "__main__":
 #
 #    n      res(n)  function  time (ms)
 #
-#    -           -        f0         -
+#   35           1        f0         -
 

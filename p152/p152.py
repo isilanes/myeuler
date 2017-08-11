810f0777e91bcbdf891e6d3fb44b6efd9bb2b751
Iñaki Silanes <isilanes@gmail.com>
2017-08-11 15:01:40 +0200

    p152: start implementing and debugging f2

--- p152/p152.py
+++ p152/p152.py
@@ -61,6 +61,7 @@ class p152(core.FunctionSet):
         """Like f0, but sorted in reverse, so some 'clever' assumption can be made.
         number_list must be ordered from smallest to largest.
         """
+
         def deintegerize(number_list):
             """From numbers (lcm/n)**2, return list of n values."""
 
@@ -72,7 +73,9 @@ class p152(core.FunctionSet):
             if not number_list: # empty list
                 return []
 
-            if number_list[0] > target:
+            leading = number_list[0]
+
+            if leading > target:
                 return []
 
             if sum(number_list) == target:
@@ -82,7 +85,7 @@ class p152(core.FunctionSet):
             for i, number in enumerate(number_list):
                 if target == number:
                     ret.append([number])
-                    continue
+                    break
 
                 subtarget = target - number
                 subnumber_list = number_list[i+1:]
@@ -107,6 +110,72 @@ class p152(core.FunctionSet):
         for group in g:
             print(group)
 
+    def f2(self, n):
+        """Like f0, with memoization."""
+
+        CACHE = {}
+
+        def deintegerize(number_list):
+            """From numbers (lcm/n)**2, return list of n values."""
+
+            return [int(lcm/math.sqrt(val)) for val in number_list]
+
+        def groups(target, number_list):
+            """Return list of all subsets of integers in 'number_list' that add to 'target'."""
+            print("DEBUG125", target)
+
+            if not number_list: # empty list
+                return []
+
+            leading = number_list[0]
+            key = (target, leading)
+
+            if key in CACHE:
+                return CACHE[key]
+
+            #if leading > target:
+            #    CACHE[key] = []
+            #    return []
+
+            if sum(number_list) == target:
+                CACHE[key] = [number_list]
+                return [number_list]
+
+            ret = []
+            for i, number in enumerate(number_list):
+                if target == number:
+                    ret.append([number])
+                    break
+
+                subtarget = target - number
+                subnumber_list = number_list[i+1:]
+                for group in groups(subtarget, subnumber_list):
+                    print("DEBUG40", deintegerize(subgroup))
+                    if group:
+                        subgroup = [number]
+                        subgroup.extend(group)
+                        print("DEBUG42", deintegerize(subgroup))
+                        ret.append(subgroup)
+
+            CACHE[key] = ret
+
+            print(CACHE)
+            exit()
+            return ret 
+
+
+        lcm = core.lcm_of_many(range(2, n))
+        #elements = [(lcm // e)**2 for e in range(n, 1, -1)]
+        elements = [(lcm // e)**2 for e in range(2, n+1)]
+        TARGET = lcm**2 // 2
+
+        print(TARGET)
+        print(deintegerize(elements))
+        g = groups(TARGET, elements)
+        for group in g:
+            print(group)
+
+
 
 # Main code:
 if __name__ == "__main__":
@@ -117,5 +186,27 @@ if __name__ == "__main__":
 #
 #    n      res(n)  function  time (ms)
 #
-#   35           1        f0         -
+#   10           -        f0        0.6
+#   20           -        f0      320 
+#   25           -        f0     9400
+#   30           -        f0        -
+#   35           1        f0        -
 
+# pypy 5.1.2 @ Python 2.7.10
+#
+#    n      res(n)  function  time (ms)
+#   10           -        f0        2.3
+#   20           -        f0      210 
+#   25           -        f0     2700
+#   30           -        f0        -
+#
+#   10           -        f1        2.2
+#   20           -        f1      165 
+#   25           -        f1     2700
+#   30           -        f1        -
+#
+#   10           -        f2        1.0
+#   20           -        f2        0.5 
+#   25           -        f2        0.8
+#   30           -        f2        1.9
+#   35           1        f2        1.9

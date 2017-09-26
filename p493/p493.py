f2539027c78fc4b73c334e8ef7ddb7d323fb519a
isilanes <isilanes@gmail.com>
2017-09-26 21:37:01 +0200

    p493: thought I fixed a bug, but still wrong result

--- p493/p493.py
+++ p493/p493.py
@@ -55,48 +55,119 @@ class p493(core.FunctionSet):
         return ave, diff
 
     def f1(self, n=None):
-        # Probability to pick 6 colors:
-        # p6 = c1 * p(not A)
+        """Makes no sense why it doesn't work. Maybe precission thing."""
+
+        # Probability to pick 6 colors OR LESS:
+        # p6less = c1 * p(not A)
         # where:
         # c1 = amount of combinations of 1 color (7)
         # p(not A) = probability of not picking color A
-        p6 = 7
+        p6less = 7
         for i in range(20):
-            p6 *= (60-i)/(70-i)
+            p6less *= (60.-i)/(70-i)
 
-        # Probability to pick 5 colors:
-        # p5 = c2 * p(not A, not B)
+        # Probability to pick 5 colors OR LESS:
+        # p5less = c2 * p(not A, not B)
         # c2 = 6+5+4+3+2+1 = 21
-        p5 = 21
+        p5less = 21
         for i in range(20):
-            p5 *= (50-i)/(70-i)
+            p5less *= (50.-i)/(70-i)
 
-        # Probability to pick 4 colors:
-        # p4 = c3 * p(not A, not B, not C)
+        # Probability to pick 4 colors OR LESS:
+        # p4less = c3 * p(not A, not B, not C)
         # c3 = 35
-        p4 = 35
+        p4less = 35
         for i in range(20):
-            p4 *= (40-i)/(70-i)
+            p4less *= (40.-i)/(70-i)
 
-        # Probability to pick 3 colors:
-        # p3 = c4 * p(not A, not B, not C, not D)
+        # Probability to pick 3 colors OR LESS:
+        # p3less = c4 * p(not A, not B, not C, not D)
         # c3 = c4 = 35
-        p3 = 35
+        p3less = 35
         for i in range(20):
-            p3 *= (30-i)/(70-i)
+            p3less *= (30.-i)/(70-i)
 
-        # Probability to pick 2 colors:
-        # p2 = c5 * p(not A, not B, not C, not D, not E)
+        # Probability to pick 2 colors OR LESS:
+        # p2less = c5 * p(not A, not B, not C, not D, not E)
         # c5 = c2 = 21
-        p2 = 21
+        p2less = 21
         for i in range(20):
-            p2 *= (20-i)/(70-i)
+            p2less *= (20.-i)/(70-i)
+
+        # Probability to pick just 2 colors equal to 2 colors or less (p(less)=0):
+        p2 = p2less
+
+        # Probability to pick just N colors is N or less minus (N-1) or less:
+        p3 = p3less - p2less
+        p4 = p4less - p3less
+        p5 = p5less - p4less
+        p6 = p6less - p5less
 
         # Probability to pick 7 colors:
-        p7 = 1 - p6 - p5 - p4 - p3 - p2
+        p7 = 1 - p6less
 
         # Expected value:
         expected = 7*p7 + 6*p6 + 5*p5 + 4*p4 + 3*p3 + 2*p2
+
+        return "{e:.9f}".format(e=expected)
+
+    def f2(self, n=None):
+        """Exactly as f1, but more accurate (lose less decimals).
+        Preserving decimals above and beyond duty, exact same (wrong) result.
+        """
+        # Probability to pick 6 colors OR LESS:
+        # p6less = c1 * p(not A)
+        # where:
+        # c1 = amount of combinations of 1 color (7)
+        # p(not A) = probability of not picking color A
+        num6less = 7
+        div = 1
+        for i in range(20):
+            num6less *= (60.-i)
+            div *= (70-i)
+
+        # Probability to pick 5 colors OR LESS:
+        # p5less = c2 * p(not A, not B)
+        # c2 = 6+5+4+3+2+1 = 21
+        num5less = 21
+        for i in range(20):
+            num5less *= (50.-i)
+
+        # Probability to pick 4 colors OR LESS:
+        # p4less = c3 * p(not A, not B, not C)
+        # c3 = 35
+        num4less = 35
+        for i in range(20):
+            num4less *= (40.-i)
+
+        # Probability to pick 3 colors OR LESS:
+        # p3less = c4 * p(not A, not B, not C, not D)
+        # c3 = c4 = 35
+        num3less = 35
+        for i in range(20):
+            num3less *= (30.-i)
+
+        # Probability to pick 2 colors OR LESS:
+        # p2less = c5 * p(not A, not B, not C, not D, not E)
+        # c5 = c2 = 21
+        num2less = 21
+        for i in range(20):
+            num2less *= (20.-i)
+
+        # Probability to pick just 2 colors equal to 2 colors or less (p(less)=0):
+        num2 = num2less
+
+        # Probability to pick just N colors is N or less minus (N-1) or less:
+        num3 = num3less - num2less
+        num4 = num4less - num3less
+        num5 = num5less - num4less
+        num6 = num6less - num5less
+
+        # Probability to pick 7 colors:
+        num7 = div - num6less
+
+        # Expected value:
+        expected = (7*num7 + 6*num6 + 5*num5 + 4*num4 + 3*num3 + 2*num2) / div
 
         return "{e:.9f}".format(e=expected)
 
@@ -109,11 +180,11 @@ if __name__ == "__main__":
 
 # Benchmarks:
 benchmarks = {
-    "Python 3.6.2 (Burns)": {
-        "f0": {
-            "skip": False,
+    "Python 3.5.2 (Skinner)": {
+        "f1": {
+            "skip": True,
             "data": [ # n, result, time (ms)
-                [ 2,                0,     0.01 ],
+                [ 7, 6.812598295, 0.1 ],
             ],
         },
     },

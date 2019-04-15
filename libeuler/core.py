2ac85b548f260fe0b084b744bfaac50aeafd9817
isilanes <isilanes@gmail.com>
2019-04-15 20:18:25 +0200

    reuse core code by passing functions to a function, instead of subclassing superclass

--- libeuler/core.py
+++ libeuler/core.py
@@ -1,14 +1,14 @@
 # Standard libs:
 import sys
 import math
-import random
-import bisect
 import argparse
 from datetime import datetime
+
 
 # Constants:
 DEFAULT_F = "0" # which function to run, default first (zero)
 DEFAULT_N = "1" # what argument to pass to function(s), default 1
+
 
 # Functions:
 def parse_args(args=sys.argv[1:]):
@@ -17,17 +17,17 @@ def parse_args(args=sys.argv[1:]):
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-f", "--functions",
-            help="Comma-separated list of integers i, j..., to run functions fi,\
-                    fj... Default: {d}.".format(d=DEFAULT_F),
-            default=DEFAULT_F)
+                        help="Comma-separated list of integers i, j..., to run functions fi,\
+                                            fj... Default: {d}.".format(d=DEFAULT_F),
+                        default=DEFAULT_F)
 
     parser.add_argument("-n", "--nvals",
-            help="Comma-separated list of integers n, m..., to run functions fi(n),\
-                    fi(m)... Default: {d}.".format(d=DEFAULT_N),
-            default=DEFAULT_N)
-
+                        help="Comma-separated list of integers n, m..., to run functions fi(n),\
+                                            fi(m)... Default: {d}.".format(d=DEFAULT_N),
+                        default=DEFAULT_N)
 
     return parser.parse_args(args)
+
 
 def factors_of(n):
     """Return list of prime factors of n.
@@ -47,12 +47,13 @@ def factors_of(n):
             factors.append(factor)
             n /= factor
         else:
-            factor += 2 # recall N will always be prime (think about it)
+            factor += 2  # recall N will always be prime (think about it)
             if factor*factor > n: # if so, the remainder N is prime already
                 factors.append(int(n))
                 break
 
     return factors
+
 
 def factors_of_with_primes(n, primes):
     """Return list of prime factors of n.
@@ -67,20 +68,21 @@ def factors_of_with_primes(n, primes):
         n /= 2
 
     # All the rest (odd):
-    ifactor = 1 # ifactor-th prime in list (0 -> 2, 1 -> 3, etc)
+    i_factor = 1  # i_factor-th prime in list (0 -> 2, 1 -> 3, etc)
     while n > 1:
-        prime = primes[ifactor]
+        prime = primes[i_factor]
         if not n % prime:
             factors.append(prime)
             n /= prime
         else:
-            ifactor += 1
-            prime = primes[ifactor]
+            i_factor += 1
+            prime = primes[i_factor]
             if prime*prime > n: # if so, the remainder N is prime already
                 factors.append(int(n))
                 break
 
     return factors
+
 
 def gcd(n, m):
     """Greatest common divisor of integers n and m.
@@ -96,6 +98,7 @@ def gcd(n, m):
     else:
         return m
 
+
 def lcm(n, m):
     """Least common multiple of integers n and m.
     Uses reduction by the GCD.
@@ -106,6 +109,7 @@ def lcm(n, m):
         return n // gcd(n, m) * m
     else:
         return m // gcd(n, m) * n
+
 
 def lcm_of_many(number_list):
     """Least common multiple of all the integers in 'number_list'."""
@@ -121,6 +125,7 @@ def lcm_of_many(number_list):
         _lcm = lcm(_lcm, number)
 
     return _lcm
+
 
 def is_prime(n):
     """Returns True if n is prime. False otherwise."""
@@ -138,6 +143,44 @@ def is_prime(n):
     return True
 
 
+def run_functions(all_functions):
+    """Perform a run of all pertinent solutions."""
+
+    # Parse command-line arguments:
+    opts = parse_args()
+
+    # List of functions:
+    function_indices = []
+    if opts.functions:
+        function_indices = [int(i) for i in opts.functions.split(",")]
+
+    # List of N values:
+    ns = []
+    if opts.nvals:
+        ns = [int(i) for i in opts.nvals.split(",")]
+
+    # Execute all functions with all N values:
+    times = []
+    for i in function_indices:
+        for n in ns:
+            now = datetime.now()
+
+            print("--- f{i}({n}) ---".format(i=i, n=n))  # print function name
+
+            print(all_functions[i](n))
+
+            dt = (datetime.now() - now).total_seconds()
+            times.append([[i,n], dt])
+
+    # Print out times:
+    print("\nTimes:\n")
+    for (i,n),t in times:
+        if t < 2:
+            print('f{i}({n}): {t:.1f} ms'.format(i=i, t=t*1000, n=n))
+        else:
+            print('f{i}({n}): {t:.1f} s'.format(i=i, t=t, n=n))
+
+
 # Classes:
 class FunctionSet(object):
     """Parent class for all solution sets."""
@@ -145,7 +188,6 @@ class FunctionSet(object):
     # Constructor:
     def __init__(self, disp=True):
         self.disp = disp
-
 
     # Public methods:
     def f0(self, n):
--- p650/p650.py
+++ p650/p650.py
@@ -7,6 +7,23 @@ from libeuler import core
 
 
 # Functions:
+def f0(n):
+    ret = 0
+
+    for j in range(1, n+1):
+        prime_factors = {}
+
+        for i in additive_factorials(j):
+            add_prime_factors_of_factorial(prime_factors, i)
+
+        for i in subtractive_factorials(j):
+            remove_prime_factors_of_factorial(prime_factors, i)
+
+        ret += sum_of_divisors(prime_factors)
+
+    return ret
+
+
 def prime_divisors_of(n):
     if n == 2:
         return {2: 1}
@@ -63,51 +80,25 @@ def subtractive_factorials(n):
     yield n-1
 
 
-# Classes:
-class p650(core.FunctionSet):
-    """Group of solutions."""
-    
-    prime_factors = {}
+def add_prime_factors_of_factorial(prime_factors, n):
+    for i in range(2, n+1):
+        for p, exp in prime_divisors_of(i).items():
+            prime_factors[p] = prime_factors.get(p, 0) + exp
 
-    # Solutions:
-    def f0(self, n):
-        
-        ret = 0
-        
-        for j in range(1, n+1):
-            self.prime_factors = {}
-        
-            for i in additive_factorials(j):
-                self.add_prime_factors_of_factorial(i)
-            
-            for i in subtractive_factorials(j):
-                self.remove_prime_factors_of_factorial(i)
-            
-            #print(self.prime_factors)
-            #print(self.sum_of_divisors())
-            
-            ret += self.sum_of_divisors()
-        
-        return ret
-        
-    def add_prime_factors_of_factorial(self, n):
-        for i in range(2, n+1):
-            for p, exp in prime_divisors_of(i).items():
-                self.prime_factors[p] = self.prime_factors.get(p, 0) + exp
-                
-    def remove_prime_factors_of_factorial(self, n):
-        for i in range(2, n+1):
-            for p, exp in prime_divisors_of(i).items():
-                self.prime_factors[p] -= exp  # it is an error if p was not a divisor, anyway
-    
-    def sum_of_divisors(self):
-        return sum(divisor_list_of_factors(self.prime_factors))
-    
+
+def remove_prime_factors_of_factorial(prime_factors, n):
+    for i in range(2, n+1):
+        for p, exp in prime_divisors_of(i).items():
+            prime_factors[p] -= exp  # it is an error if p was not a divisor, anyway
+
+
+def sum_of_divisors(prime_factors):
+    return sum(divisor_list_of_factors(prime_factors))
+
 
 # Main code:
 if __name__ == "__main__":
-    P = p650()
-    P.run()
+    core.run_functions([f0])
 
 # Python 3.7.3 times (Fry)
 #

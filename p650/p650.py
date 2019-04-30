4b6b37db96bfc59c804d45e78300c7753c4ef765
Iñaki Silanes <isilanes@lortek.es>
2019-04-30 12:09:30 +0200

    deleted old stuff and polished f1

--- p650/p650.py
+++ p650/p650.py
@@ -6,6 +6,10 @@ sys.path.append("..")
 from libeuler import core
 
 
+# Globals:
+MOD_NUMBER = 1_000_000_007
+
+
 # Functions:
 def p650(n):
     ret = 1
@@ -22,28 +26,11 @@ def p650(n):
             for divisor, power in prime_divisors_of(k).items():
                 prime_factors[divisor] = prime_factors.get(divisor, 0) + v*power
                 
-        ret += sum_of_divisors(prime_factors) % 1000000007
+        ret += sum_of_divisors(prime_factors) % MOD_NUMBER
         
-    return ret
+    return ret % MOD_NUMBER
     
     
-def old(n):
-    ret = 0
-    
-    for j in range(1, n+1):
-        prime_factors = {}
-
-        for i in additive_factorials(j):
-            add_prime_factors_of_factorial(prime_factors, i)
-
-        for i in subtractive_factorials(j):
-            remove_prime_factors_of_factorial(prime_factors, i)
-
-        ret += sum_of_divisors(prime_factors) % 1000000007
-
-    return ret
-
-
 def prime_divisors_of(n):
     divisors = {}
     for factor in core.factors_of(n):
@@ -52,47 +39,12 @@ def prime_divisors_of(n):
     return divisors
 
 
-def divisor_list_of_factors(factors):
-    divisors = [1]
-    for factor, exponent in factors.items():
-        tmp = []
-        tmp2 = [factor**i for i in range(exponent+1)]
-        for d in divisors:
-            tmp.extend([d*t for t in tmp2])
-        divisors = tmp[:]
-    
-    return divisors
-
-
-def additive_factorials(n):
-    for i in range(n-1):
-        yield n
-    
-    
-def subtractive_factorials(n):
-    yield n-1
-        
-    for i in range(1, n-2):
-        yield n-i-1
-        yield i+1
-        
-    yield n-1
-
-
-def add_prime_factors_of_factorial(prime_factors, n):
-    for i in range(2, n+1):
-        for p, exp in prime_divisors_of(i).items():
-            prime_factors[p] = prime_factors.get(p, 0) + exp
-
-
-def remove_prime_factors_of_factorial(prime_factors, n):
-    for i in range(2, n+1):
-        for p, exp in prime_divisors_of(i).items():
-            prime_factors[p] -= exp  # it is an error if p was not a divisor, anyway
-
-
 def sum_of_divisors(prime_factors):
-    return sum(divisor_list_of_factors(prime_factors))
+    result = 1
+    for k, v in prime_factors.items():
+        result *= (k**(v+1) - 1) // (k - 1)
+    
+    return result
 
 
 # Main code:
@@ -103,8 +55,19 @@ if __name__ == "__main__":
 #
 #    n       res(n)  function  time (ms)
 #    5         5736        f0        0.1
-#   10   1721034274        f0        1.6
-#   15   5198581147        f0       37.6
-#   20   7131742875        f0     7100
+#   10   1721034274        f0        1.6 <-- wrong (bug)
+#   15   5198581147        f0       37.6 <-- wrong (bug)
+#   20   7131742875        f0     7100   <-- wrong (bug)
 #   22  MemoryError
-
+#
+#    n       res(n)  function  time (ms)
+#    5         5736        f1        0.1
+#   10    721034267        f1        0.2
+#   15    198581112        f1        0.6
+#   20    131742826        f1        1.2
+#   50    217147306        f1       10.2
+#  100    332792866        f1       73.1
+#  200    271664942        f1      449.4
+#  500    899393748        f1     7600
+#  750     75285818        f1    31500
+# 1000    361160563        f1    87900

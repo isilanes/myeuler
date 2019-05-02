332ed338e1575a6354d71b3847a13b2934c5de0d
isilanes <isilanes@gmail.com>
2019-05-02 20:21:26 +0200

    p205: solved

--- p205/p205.py
+++ p205/p205.py
@@ -12,26 +12,76 @@ from libeuler import core
 # Functions:
 def p205(n=None):
     # 6x 6-sided dice:
-    count_six = [0 for _ in range(6 * 4 + 1)]
+    count_six = get_count_six()
+
+    # Cumulative 6x 6-sided dice:
+    cumulative_six = get_cumulative_six(count_six)
+
+    # 9x 4-sided dice:
+    count_four = get_count_four()
+
+    fours_above = 0  # how many times 9x 4-sided dice roll is above 6x 6-sided
+    for i, n in enumerate(count_four):
+        c = count_six_less_than(cumulative_six, i)
+        fours_above += c*n
+
+    return round(fours_above/(4**9 * 6**6), 7)
+
+
+def get_count_six():
+    """6x 6-sided dice."""
+
+    count_six = [0 for _ in range(6 * 6 + 2)]
     for i_first in range(1, 7):
         for i_second in range(1, 7):
             for i_third in range(1, 7):
                 for i_fourth in range(1, 7):
-                    r = i_first + i_second + i_third + i_fourth
-                    count_six[r] += 1
-    
-    print(count_six)
-    
-    # Cumulative 6x 6-sided dice:
-    cumulative_six = [0 for _ in range(6 * 4 + 1)]
+                    for i_fifth in range(1, 7):
+                        for i_sixth in range(1, 7):
+                            r = i_first + i_second + i_third + i_fourth + i_fifth + i_sixth
+                            count_six[r] += 1
+
+    return count_six
+
+
+def get_cumulative_six(count):
+    """Cumulative 6x 6-sided dice."""
+
+    cumulative_six = [0 for _ in range(6 * 6 + 2)]
     so_far = 0
-    for i, val in enumerate(count_six):
+    for i, val in enumerate(count):
         cumulative_six[i] = so_far
         so_far += val
-    print(cumulative_six)
-    
-    # Prob n+:
-    print(n, 1.0 - cumulative_six[n]/sum(count_six))
+
+    return cumulative_six
+
+
+def count_six_less_than(cumulative, n):
+    """How many occurrences of a 6x 6-sided die-roll being LESS than 'n',
+    out of the 6**6 combinations.
+    """
+
+    return cumulative[n]
+
+
+def get_count_four():
+    """9x 4-sided dice."""
+
+    count_four = [0 for _ in range(9 * 4 + 2)]
+    for i_first in range(1, 5):
+        for i_second in range(1, 5):
+            for i_third in range(1, 5):
+                for i_fourth in range(1, 5):
+                    for i_fifth in range(1, 5):
+                        for i_sixth in range(1, 5):
+                            for i_seventh in range(1, 5):
+                                for i_eighth in range(1, 5):
+                                    for i_ninth in range(1, 5):
+                                        r = i_first + i_second + i_third + i_fourth + i_fifth + i_sixth \
+                                            + i_seventh + i_eighth + i_ninth
+                                        count_four[r] += 1
+
+    return count_four
 
 
 # Main code:
@@ -41,5 +91,9 @@ if __name__ == "__main__":
 # Python 3.7.3 times (Manjaro)
 #
 #    n       res(n)  function  time (ms)
-#    -
-
+#    -                     f0       62.8
+#
+# pypy3 7.1.0-beta0 times (Manjaro)
+#
+#    n       res(n)  function  time (ms)
+#    -                     f0       23.6

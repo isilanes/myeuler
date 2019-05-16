af02d7d984e4c97fc5e394485b08b793bbd1cbc4
Iñaki Silanes <isilanes@lortek.es>
2019-05-16 17:45:15 +0200

    p233: started

---
+++ p233/p233.py
@@ -0,0 +1,23 @@
+# Standard libs:
+import sys
+
+# Our libs:
+sys.path.append("..")
+from libeuler import core
+
+
+# Globals:
+
+
+# Functions:
+def f0(n=None):
+    pass
+    
+    
+# Main code:
+if __name__ == "__main__":
+    core.run_functions([f0])
+
+# PyPy 5.10.0 times (Manjaro)
+#
+#           n              res(n)  function  time (ms)

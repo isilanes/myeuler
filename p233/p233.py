8fa35fc84034ce20c544c711fe77823653ade4f2
Iñaki Silanes <isilanes@lortek.es>
2019-12-20 12:31:36 +0100

    p233: some changes done long time ago

--- p233/p233.py
+++ p233/p233.py
@@ -6,18 +6,23 @@ import math
 sys.path.append("..")
 from libeuler import core
 
+# Globals:
+K_XMAX = 0.5 + 2**-0.5  # x_max = N * K_XMAX
+
 
 # Functions:
 def f0(n=None):
     max_np = 0
-    for N in range(2, n):
+    for N in range(2, n+1):
         np = 0
-        for dx in range(N//2):
-            x = int(N/2 + dx)
+        xmax = int(K_XMAX * N)
+        for x in range(N+1, xmax+1):
             y = (math.sqrt(N**2 - 4*x**2 + 4*x*N) + N)*0.5
-            y = int(y)
-            if 2*N**2 == (2*x - N)**2 + (2*y - N)**2:
+            if not y % 1:
                 np += 1
+            #y = int(y)
+            #if 2*N**2 == (2*x - N)**2 + (2*y - N)**2:
+                #np += 1
         
         if np > 0:
             np = np*8 + 4
